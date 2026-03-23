9281f87d124f9b08c2b0b315864ae9a85eb939e2
Yury Zholobov <zholobov@gmail.com>
2026-03-22 18:54:43 -0700

    fix: create and register ECS World before adding systems

--- src/levels/test_level.gd
+++ src/levels/test_level.gd
@@ -3,6 +3,12 @@ extends Node3D
 const HUDScene = preload("res://src/ui/hud.tscn")
 
 func _ready():
+    # Create and register the ECS world
+    var world = World.new()
+    world.name = "World"
+    add_child(world)
+    ECS.world = world
+
     ECS.world.add_system(S_PlayerInput.new())
     ECS.world.add_system(S_Movement.new())
 
@@ -10,4 +16,4 @@ func _ready():
     add_child(hud)
 
 func _physics_process(delta: float) -> void:
-    ECS.world.process(delta)
+    ECS.process(delta)
