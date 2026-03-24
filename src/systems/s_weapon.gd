361bad19401f65804606938e542014fcb4ef256d
Yury Zholobov <zholobov@gmail.com>
2026-03-24 10:20:25 -0700

    feat: add weapon component and firing system

---
+++ src/systems/s_weapon.gd
@@ -0,0 +1,23 @@
+class_name S_Weapon
+extends System
+
+signal projectile_requested(owner_body: Node3D, weapon: C_Weapon)
+
+func query() -> QueryBuilder:
+    return q.with_all([C_Weapon, C_NetworkIdentity])
+
+func process(entities: Array[Entity], _components: Array, delta: float) -> void:
+    for entity in entities:
+        var weapon := entity.get_component(C_Weapon) as C_Weapon
+        var net_id := entity.get_component(C_NetworkIdentity) as C_NetworkIdentity
+
+        # Tick cooldown
+        if weapon.cooldown_remaining > 0:
+            weapon.cooldown_remaining -= delta
+
+        # Fire if requested and ready
+        if weapon.is_firing and weapon.cooldown_remaining <= 0 and net_id.is_local:
+            weapon.cooldown_remaining = weapon.fire_rate
+            var body = entity.get_parent()
+            if body:
+                projectile_requested.emit(body, weapon)
